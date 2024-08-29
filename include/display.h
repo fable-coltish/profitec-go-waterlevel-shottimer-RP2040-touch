f1ee9d0d3aa8475a064895705bcecb0090a615e3
Jonny <fable-coltish.0v@icloud.com>
2024-08-29 18:13:09 +0800

    Changed LCD_RST_PIN designation

--- include/display.h
+++ include/display.h
@@ -10,7 +10,7 @@
 #define LCD_CS_PIN      (9)
 #define LCD_CLK_PIN     (10)
 #define LCD_MOSI_PIN    (11)
-#define LCD_RST_PIN     (12)
+#define LCD_RST_PIN     (13)
 #define LCD_BL_PIN      (25)
 
 // Display dimensions
